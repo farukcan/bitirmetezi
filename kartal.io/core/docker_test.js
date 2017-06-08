12a516b328584d6745907cf6b1f42675355b2e0b
farukcan <120502006@istanbulticaret.edu.tr>
2017-06-08 17:02:32 +0300

    Create docker_test.js

--- kartal.io/core/docker_test.js
+++ kartal.io/core/docker_test.js
@@ -1,4 +1,4 @@
-console.log("TESTING");
+console.log("TESTING 15sec");
 eval(require("fs").readFileSync("app.js", 'utf8'));
-setTimeout(process.exit,5000);
+setTimeout(process.exit,15000);
 console.log("TEST PASSED");
